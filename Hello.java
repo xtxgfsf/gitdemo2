49398c17ffc5e139b12c63ae2c0edd8c155d40c1
付淑芬 <870354035@qq.com>
2023-03-17 03:48:13 +0000

    online update Hello.java.
    Signed-off-by: 付淑芬 <870354035@qq.com>

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello Git1");
 		System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
 	}
 }
 
